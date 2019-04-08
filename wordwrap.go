b209ef58de27eff55b4868d58de6a6d87890d1fd
FreshStone <abhishekdixit271993@gmail.com>
2019-04-08 19:22:36 +0530

    optimised printlines()

--- wordwrap.go
+++ wordwrap.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"math"
 )
-var a = []int{6,1,3,1} //len of consecutive words
+//var a = []int{6,1,3,1}; var w = 8 //len of consecutive words
+var a = []int{3,2,2,5}
 var index = make([]int, len(a))
-var w = 8 //max width of each line
-//var top = -1
+var w = 5 //max width of each line
+
 
 func main(){
 	dp := make([]int, len(a)+1)
@@ -17,6 +18,7 @@ func main(){
 		n = a[i-1]
 		min_cost = dp[i-1] + int(math.Pow(float64(w-n), 3))
 		j := i-1
+		index[i-1] = j
 		Loop :
 			for ; j >=1; j-- {
 				n = n + a[j-1] +1
@@ -26,35 +28,21 @@ func main(){
 				}
 				if min_cost >  dp[j-1] + int(math.Pow(float64(w-n), 3)){
 					min_cost = dp[j-1] + int(math.Pow(float64(w-n), 3))
-					index[i-1] = j// 1st word 0 index
+				//	index[i-1] = j
+					index[i-1] = j-1
 				}
 			}
 		dp[i] = min_cost
 	}
 	fmt.Println("min_cost-", dp[len(a)])
-	fmt.Println("index array-", index)
-	Printlines()
+	printlines()
 }
 
-func Printlines(){ //optimize
-	var s []int
-	line_num := 1
-	for i := len(index)-1; i>=0;{
-		if index[i] == 0{
-			index[i] = i+1
-		}
-		s = append(s, index[i])
-		i = index[i]-2
-	}
-	
-	for j := len(s)-1; j >= 0; j--{
-		if j == 0{
-			fmt.Printf("line-num %d starting word %d ending word %d\n", line_num, s[j], len(a))
-		}else{
-			fmt.Printf("line-num %d starting word %d ending word %d\n", line_num, s[j], (s[j-1] - 1))
-		}
-		line_num += 1
+func printlines(){
+	j := len(index)-1
+	for ; j >= 0;{
+		defer fmt.Printf("starting word %d ending word %d\n", index[j]+1, j+1)
+		j = index[j]-1
 	}
 }
-
 
